bc28e3010adde0916f204c39eb35f2376e6aff01
Faiz Khan <183866492+FaizK23@users.noreply.github.com>
2026-03-11 13:39:15 -0500

    Added devlog, App.java, and calculator.java calculator.java hosts all the propeties of the calculator program App.java is the execulatble file

---
+++ App.java
@@ -0,0 +1,9 @@
+import java.net.CacheRequest;
+
+public class App {
+    public static void main(String[] args) {
+        calculator calculator = new calculator(); // creating an instance of calculator class
+
+        
+    }
+}
---
+++ calculator.java
@@ -0,0 +1,66 @@
+// march 11 2026
+// making a simple calculator project using java
+// created by Faiz Khan
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class calculator{
+    // dimensions of the window
+    int boardWidth = 360;
+    int boardHeight = 540;
+    
+    // setting up colors for the calulator 
+    Color lightGray = new Color(212,212,210);
+    Color darkGray = new Color(80,80,80);
+    Color black = new Color(28,28,28);
+    Color orange = new Color(255,149,0);
+
+    JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPlanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+
+    // creating arrays for button values
+    String[] buttonValues = {"AC", "+/-", "%", "/",
+                             "7",  "8", "9", "x",
+                             "4", "5", "6", "-",
+                             "1", "2", "3", "+",
+                             "0", "."," sqrt", "="
+    };
+
+    String[] rightSymbols = {"/", "x", "-", "+", "="};
+    String[] topSymbols = {"AC", "+/-", "%"};
+
+
+    // setting up the properties of the window
+    calculator(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        // setting up label elements for the result 
+        displayLabel.setBackground(black);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        // making label elements veiwable
+        displayPlanel.setLayout(new BorderLayout());
+        displayPlanel.add(displayLabel);
+        frame.add(displayPlanel, BorderLayout.NORTH); // setting up result space
+        // making the button elements veiwable
+        buttonPanel.setLayout(new GridLayout(5,4));
+        buttonPanel.setBackground(black);
+        frame.add(buttonPanel);
+
+    }
+}
