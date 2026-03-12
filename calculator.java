eafc5c62477df398c43d63fd5269c04952d8348a
Faiz Khan <183866492+FaizK23@users.noreply.github.com>
2026-03-12 13:41:45 -0500

    Updated the devlog and Updated calculator.java I have gotten the buttons to display, and for the numerical buttons and the clearning buttons to work

--- calculator.java
+++ calculator.java
@@ -29,11 +29,16 @@ public class calculator{
                              "7",  "8", "9", "x",
                              "4", "5", "6", "-",
                              "1", "2", "3", "+",
-                             "0", "."," sqrt", "="
+                             "0", ".","√", "="
     };
 
     String[] rightSymbols = {"/", "x", "-", "+", "="};
     String[] topSymbols = {"AC", "+/-", "%"};
+
+    // keeps track of 2 numbers and the opperator : A+B A-B
+    String A = "0";
+    String operator = null;
+    String B = null;
 
 
     // setting up the properties of the window
@@ -62,5 +67,89 @@ public class calculator{
         buttonPanel.setBackground(black);
         frame.add(buttonPanel);
 
+        // using a for loop to display the buttons
+        for(int i = 0; i < buttonValues.length; i++){
+            // setting up the button properties
+            JButton button = new JButton();
+            String buttonValue = buttonValues[i];
+            button.setFont(new Font("Arial", Font.PLAIN, 30));
+            button.setText(buttonValue);
+
+            // only way to get the buttons colored on mac
+            button.setOpaque(true);
+            button.setBorderPainted(false);
+            button.setFocusPainted(false);
+
+            button.setBorder(new LineBorder(Color.blue));
+
+            // using conditionals to assign color to certain buttons
+            if(Arrays.asList(topSymbols).contains(buttonValue)){
+                button.setBackground(lightGray);
+                button.setForeground(black);
+            }
+            else if(Arrays.asList(rightSymbols).contains(buttonValue)){
+                button.setBackground(orange);
+                button.setForeground(Color.white);
+            }
+            else{
+                button.setBackground(Color.darkGray);
+                button.setForeground(Color.white);
+            }
+
+            buttonPanel.add(button); // adding button to the pannel
+
+            // setting up the buttons so that they work when clicked
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    JButton button = (JButton) e.getSource(); // getting the source of click
+                    String buttonValue = button.getText();
+
+                    if(Arrays.asList(rightSymbols).contains(buttonValue)){
+                        
+                    }
+                    else if(Arrays.asList(topSymbols).contains(buttonValue)){
+                        if(buttonValue == "AC"){ // clearing
+                            clearAll();
+                            displayLabel.setText("0");
+                        }
+                        else if(buttonValue == "+/-"){ // making the value in displayLabel negative or positive
+                            double numDisplay = Double.parseDouble(displayLabel.getText());
+                            numDisplay *= -1;
+                            displayLabel.setText(removeZeroDecimal(numDisplay));
+                        }
+                        else if(buttonValue == "%"){
+
+                        }
+                    }
+                    else{   // digits or .
+                        if (buttonValue == "."){
+                            if(!displayLabel.getText().contains(buttonValue)) // only 1 . can be used
+                                displayLabel.setText(displayLabel.getText() + buttonValue);
+                        }
+                        else if("0123456789".contains(buttonValue)){
+                            if(displayLabel.getText() == "0") // replace 0 with button value
+                                displayLabel.setText(buttonValue);
+                            else    // append multiple button values
+                                displayLabel.setText(displayLabel.getText() + buttonValue);
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+
+    void clearAll(){
+        A = "0";
+        operator = null;
+        B = null;
+    }
+
+    String removeZeroDecimal(double numDisplay){
+        if(numDisplay % 1 == 0){
+            return Integer.toString((int) numDisplay);
+        }
+        else
+            return Double.toString(numDisplay);
     }
 }
